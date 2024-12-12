e4d2e0c7d53d2522b8fa739e1040d30fdbee0a42
markmit211 <markmit@mit.edu>
2024-12-12 13:18:46 -0500

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -247,9 +247,9 @@ class cage_mount_pair:
 
         # Importing surface adapters:
         # Leftmost adapter:
-        _add_linked_object(obj, "cage_mount_adapter", cage_mount_adapter, pos_offset=(1.397, 0, -13.97), rot_offset=(0, 0, 90))
+        _add_linked_object(obj, "cage_mount_adapter", cage_mount_adapter, pos_offset=(1.397, 0, height-13.97), rot_offset=(0, 0, 90))
         # Rightmost adapter:
-
+        _add_linked_object(obj, "cage_mount_adapter", cage_mount_adapter, pos_offset=(1.397, -spread, height-13.97), rot_offset=(0, 0, 90))
 
     # this defines the component body and drilling
     def execute(self, obj):
