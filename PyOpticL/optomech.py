93f0a909774957bd0a9953c33d9648fd1cefcebd
markmit211 <markmit@mit.edu>
2025-02-06 20:59:38 -0500

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -216,8 +216,8 @@ class drill_test:
 
         # part = _custom_box(dx=80, dy=80, dz=15+12.7, 
         #                    x=0, y=35, z=-12.7, fillet=5)
-        part = _custom_cylinder(dia=2.5, dz=30, x=0, y=0, z=0)
-        part = part.fuse(_custom_cylinder(dia=5, x=0, y=0, z=-6))
+        part = _custom_cylinder(dia=2.5, dz=50, x=0, y=0, z=0)
+        part = part.fuse(_custom_cylinder(dia=5, dz=44, x=0, y=0, z=-6))
 
         obj.Shape = part
 
