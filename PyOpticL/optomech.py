7ae5c565c95ebbc7a67f2aa14fef1a6e7e70ffb7
markmit211 <markmit@mit.edu>
2025-02-21 12:15:52 -0500

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -211,7 +211,7 @@ class drill_test:
         # spread = obj.Spread.Value
         # part = _custom_box(dx=spread+0.35*3*inch, dy=1.8*inch, dz=16, 
         #                    x=-0.5*(spread+0.35*inch), y=0, z=-(20.32+5/16*inch), fillet=5)
-        part = _custom_box(dx=75+10, dy=96.25+10, dz=25.4*2, x=6.792+5.8435, y=34, z=-13.1, fillet=5)
+        part = _custom_box(dx=75+10+2.5, dy=96.25+10, dz=25.4*2, x=6.792+5.8435-1.25, y=34, z=-13.1, fillet=5)
 
         obj.Shape = part
 
