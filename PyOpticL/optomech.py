2ad998fc8c7f2644c1518818260821dad25e9c75
markmit211 <markmit@mit.edu>
2025-05-30 10:19:40 -0400

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -268,7 +268,7 @@ class chromatic_rotation_stage:
         obj.Mesh = mesh
 
         x_off=-3.5662
-        part = _custom_box(dx=9, dy=17, dz=1.6+0.1, x=x_off, y=0, z=-12.7+0.1, dir=(0,0,-1))
+        part = _custom_box(dx=12, dy=20, dz=1.6+0.1, x=x_off, y=0, z=-12.7+0.1, dir=(0,0,-1), fillet=1)
 
         part = part.fuse(_custom_cylinder(dia=3.18, dz=6, x=x_off, y=6.35, z=-14.3, dir=(0,0,-1)))
         part = part.fuse(_custom_cylinder(dia=3.18, dz=6, x=x_off, y=-6.35, z=-14.3, dir=(0,0,-1)))
