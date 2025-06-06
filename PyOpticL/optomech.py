e584ad670850936e07f6fa95a5a1d40fe61aec9b
markmit211 <markmit@mit.edu>
2025-06-06 05:22:17 -0400

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -586,12 +586,8 @@ class isolator_895_high_power:
             post_adapter = _import_stl("RA90.stl", (0, 0, 0), (-8, 30, 37.34132+height))
             mesh.addMesh(post_adapter)
 
-            post_2 = _import_stl("POST_TR2.stl", (xrot, yrot, zrot), (xoff, yoff, zoff+height))
+            post_2 = _import_stl("POST_TR2.stl", (90, 0, 0), (-16, 30, -12.7+height))
             mesh.addMesh(post_2)
-            # post_mesh = 0
-            # post_adapter = 0
-
-
 
             mesh.Placement = obj.Mesh.Placement
             obj.Mesh = mesh
