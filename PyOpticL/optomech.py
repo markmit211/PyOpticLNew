a6fe83292ff93c6c6db29a409b157e3f6eb9ea64
markmit211 <markmit@mit.edu>
2025-06-20 12:28:08 -0400

    Update optomech.py

--- PyOpticL/optomech.py
+++ PyOpticL/optomech.py
@@ -1370,6 +1370,9 @@ class surface_adapter:
         mount_hole_dy (float) : The spacing between the two mount holes of the adapter
         adapter_height (float) : The height of the suface adapter
         outer_thickness (float) : The thickness of the walls around the bolt holes
+        adjust (bool) : Whether mounted part movement should be allowed
+        adjust_dist (float) : Amount mounted part may move in millimeters
+        tolerance : Space between surface adapter and baseplate cutout in millimeters
     '''
     type = 'Part::FeaturePython'
     def __init__(self, obj, drill=True, mount_hole_dy=20, adapter_height=8, outer_thickness=2, adjust=False, adjust_dist=5, tolerance=1):
@@ -1422,32 +1425,11 @@ class surface_adapter:
                                              x=0, y=i*obj.MountHoleDistance.Value/2, z=0))
         obj.Shape = part
 
-        # part = _bounding_box(obj, self.drill_tolerance, 6) ################# Change to include new adjustment range (Completed)
         part = _custom_box(dx=dx+fillet_tolerance, dy=dy+fillet_tolerance+adj_dist, dz=dz+10, x=0, y=0, z=-dz, fillet=dx/2, dir=(0,0,1))
-
 
         for i in [-1, 1]:
             part = part.fuse(_custom_cylinder(dia=bolt_8_32['tap_dia'], dz=drill_depth,
                                               x=0, y=i*obj.MountHoleDistance.Value/2, z=0))
-
-        # part = _custom_box(dx=dx, dy=dy, dz=dz,
-        #                    x=0, y=0, z=0, dir=(0, 0, -1),
-        #                    fillet=5)
-        # part = part.cut(_custom_cylinder(dia=bolt_8_32['clear_dia'], dz=dz,
-        #                                  head_dia=bolt_8_32['head_dia'], head_dz=bolt_8_32['head_dz'],
-        #                                  x=0, y=0, z=-dz, dir=(0,0,1)))
-        # for i in [-1, 1]:
-        #     part = part.cut(_custom_cylinder(dia=bolt_8_32['clear_dia'], dz=dz,
-        #                                      head_dia=bolt_8_32['head_dia'], head_dz=bolt_8_32['head_dz'],
-        #                                      x=0, y=i*obj.MountHoleDistance.Value/2, z=0))
-        # obj.Shape = part
-
-        # part = _bounding_box(obj, self.drill_tolerance, 6)
-        # for i in [-1, 1]:
-        #     part = part.fuse(_custom_cylinder(dia=bolt_8_32['tap_dia'], dz=drill_depth,
-        #                                       x=0, y=i*obj.MountHoleDistance.Value/2, z=0))
-        
-
 
         part.Placement = obj.Placement
         obj.DrillPart = part
